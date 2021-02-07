b3e7b85187e311e4f50c67356889e0f5aa5afa59
GRAIM-ai <59557453+GRAIM-ai@users.noreply.github.com>
2021-02-08 01:17:12 +0900

    server study

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hi");
+const express = require("express");
+const app = express();
+const PORT = 4000;
+function handleListening() {
+  console.log(`Listeng on: http://localhost:${PORT}`);
+}
+app.listen(4000, handleListening);
